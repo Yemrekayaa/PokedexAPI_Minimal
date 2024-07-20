76a85f98e34b52e4137773e9078dd2ee1eee59db
Yemrekayaa <yemrekayatr@gmail.com>
2024-07-20 16:28:53 +0300

    Add Pokemon Model

---
+++ BasicPokedex_MinimalAPI/Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicPokedex_MinimalAPI.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Index { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string SecondaryType { get; set; }
+        public string Generation { get; set; }
+    }
+}
